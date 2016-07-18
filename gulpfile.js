9557d6564bd3a1c0727154957e9983a4853ff016
david1983 <davideandreazzini@live.com>
2016-07-18 21:04:21 +0100

    added angular + ui-router and small boilerplate

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    neat = require('node-neat').includePaths;
+
 
 gulp.task('sass', function () {
   return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+            includePaths: ['styles'].concat(neat)
+        }).on('error', sass.logError))    
     .pipe(gulp.dest('./public/css'));
 });
 
---
+++ public/js/app.js
@@ -0,0 +1,50 @@
+var app = angular.module('grooco', ['ui.router']);
+
+
+var defaultRoute = {
+                navbar: { templateUrl: "/js/templates/header.tmpl.html" },
+                content: {
+                    templateUrl: "/js/templates/home.tmpl.html"
+                },
+                footer: { templateUrl: "/js/templates/footer.tmpl.html" },
+            }
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/404");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('notfound', {
+            url: "/404",
+            templateUrl: "/js/templates/404.tmpl.html",            
+        })
+        .state('home', {
+            url: "/",
+            views: Object.assign(defaultRoute, {
+                content: {
+                    templateUrl: "/js/templates/home.tmpl.html"
+                } 
+            }) 
+        })
+        .state('list', {
+            url: "/list",
+            views: Object.assign(defaultRoute, {
+                content: {
+                    templateUrl: "/js/templates/list.tmpl.html"
+                } 
+            })                               
+        })
+        .state('product', {
+            url: "/product",
+            views: Object.assign(defaultRoute, {
+                content: {
+                    templateUrl: "/js/templates/product.tmpl.html"
+                } 
+            }),
+            controller: function ($scope) {
+                $scope.things = ["A", "Set", "Of", "Things"];
+            }
+        });
+});
