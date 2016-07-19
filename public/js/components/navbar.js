383f3753aab80a93e20bdb9b849c0a69f7634572
david1983 <davideandreazzini@live.com>
2016-07-19 19:23:42 +0100

    home page template

---
+++ public/js/components/navbar.js
@@ -0,0 +1,45 @@
+app.component("navBar", {
+    templateUrl: '/js/templates/components/navbar.tmpl.html',
+    controller: function () {
+        
+        $(window).on("resize", handleMenu);
+        $(document).ready(handleMenu)
+
+    }
+})
+
+
+function handleMenu(e) {
+
+  var more = document.getElementById("js-navigation-more");
+  if ($(more).length > 0) {
+    var windowWidth = $(window).width();
+    var moreLeftSideToPageLeftSide = $(more).offset().left;
+    var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+
+    if (moreLeftSideToPageRightSide < 330) {
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
+    }
+
+    if (moreLeftSideToPageRightSide > 330) {
+      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
+      $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
+    }
+  }
+
+
+  var menuToggle = $("#js-mobile-menu").unbind();
+  $("#js-navigation-menu").removeClass("show");
+
+  menuToggle.on("click", function(e) {
+    e.preventDefault();
+    $("#js-navigation-menu").slideToggle(function(){
+      if($("#js-navigation-menu").is(":hidden")) {
+        $("#js-navigation-menu").removeAttr("style");
+      }
+    });
+  });
+ 
+
+        }
