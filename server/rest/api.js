e0223bce40ed86a4ed3493ce09c542a4f0f6b530
david1983 <davideandreazzini@live.com>
2016-07-27 01:50:37 +0100

    categories

--- server/rest/api.js
+++ server/rest/api.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var db = require('../model/db');
 var router = express.Router();
+var _ = require('lodash');
 
+router.get('/scan',function(req,res){
+    db.find('products',{}).then(function(result){
+        var categories = _.uniq(_.map(result, function(i){ return {lvl1: i.level1_category, main: i.main_category, sub: i.category}; }));  
+        var lvl1_category = _.uniq(_.map(result, function(i){ return i.level1_category}));
+        
+        var main_category =_.uniq(_.map(result, function(i){ return i.main_category}));
+        var sub_category = _.uniq(_.map(result, function(i){ return i.category}));
+        res.json({
+            lvl1: lvl1_category, main: main_category, sub: sub_category
+        })
+    }).catch(function(err){console.log(err)})
+})
 
 router.route('/:collection')
     .post(function (req, res) {
